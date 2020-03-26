a483338b1c9bcf9d943dbaa52ebd0a025035300f
Tanya <mospan_1999@mail.ru>
2020-03-27 00:14:07 +0300

    dialog box

--- app/src/main/java/com/example/quiz/Level1.java
+++ app/src/main/java/com/example/quiz/Level1.java
@@ -2,12 +2,24 @@ package com.example.quiz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.logging.Level;
 
 public class Level1 extends AppCompatActivity {
+
+    Dialog dialog;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +34,39 @@ public class Level1 extends AppCompatActivity {
 
         Window w = getWindow();
         w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+
+        //Вызов диалогового окна
+        dialog=new Dialog(this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);//скрываем заголовок
+        dialog.setContentView(R.layout.previewdialog);//путь
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));//прозрачный фон
+        dialog.setCancelable(false);//окно нельзя закрыть кнопкой назад
+
+        //закрывает диалоговое окно
+        TextView btnclose = (TextView)dialog.findViewById(R.id.btnclose);
+        btnclose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {//обработка нажатия
+               try{
+                   Intent intent=new Intent(Level1.this,GameLevels.class);
+                   startActivity(intent);
+                   finish();
+               } catch (Exception e){
+
+               }
+               dialog.dismiss();
+            }
+        });
+
+        Button btncontinue = (Button)dialog.findViewById(R.id.btncontinue) ;
+        btncontinue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+
+        dialog.show();
 
     }
 }
